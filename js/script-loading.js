119c0493bcac594a958003981f4f243b1b674765
philippe wagner <atelfoto@msn.com>
2021-01-09 15:51:55 +0100

    first commit

---
+++ js/script-loading.js
@@ -0,0 +1,11 @@
+
+window.addEventListener('DOMContentLoaded', function() {
+    $("body").queryLoader2({
+        barColor: "#00679F",
+        backgroundColor: "#FFFFFF",
+        percentage: true,
+        barHeight: 10,
+        completeAnimation: "fade",
+        minimumTime: 100
+    });
+});
